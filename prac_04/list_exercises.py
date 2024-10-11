ea015932d6aa4cc583ef556e50734c5f80a0ad71
Ryan.E <ericsonryan34@gmail.com>
2024-10-11 16:09:59 +1000

    Added smallest, largest and average output

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,4 +3,8 @@ numbers = []
 
 for i in range(5):
     numbers.append(int(input("Number: ")))
-print(f"The first number is {}")
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[4]}")
+print(f"The smallest number is {[min(numbers)]}")
+print(f"The largest number is {[max(numbers)]}")
+print(f"The average number is {[sum(numbers)/len(numbers)]}")
