5c3e8c02e8244e7f8d6a5573c691b8e43158f799
Ryan.E <ericsonryan34@gmail.com>
2024-10-12 15:13:25 +1000

    Added for loop to display rows of numbers all to the defined contstant of the number of columns

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,13 +1,27 @@
 import random
 
+
 NUMBERS_OF_COLUMNS = 6
 MAXIMUM_NUMBER = 45
 MINIMUM_NUMBER = 1
 
 
-def main():
-    rows = input("How many quick picks? ")
+row = int(input("How many quick picks? "))
+while row < 0:
+    print("Error")
+    row = int(input("How many quick picks? "))
+
+for i in range(row):
+    quick_pick = []
+    for x in range(NUMBERS_OF_COLUMNS):
+        number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        while number in quick_pick:
+            number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        quick_pick.append(number)
+    quick_pick.sort()
+    print(" ".join(f"{number:2}" for number in quick_pick))
 
 
+# print(random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER))
 
 
