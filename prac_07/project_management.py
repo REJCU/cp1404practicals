abe7df1ee7ef6170410c1c224ae3837f0013f551
Ryan.E <ericsonryan34@gmail.com>
2024-11-08 16:05:43 +1000

    Feat: added the ability to load projects from external txt documents

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -7,14 +7,15 @@ MENU = ("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects"
 
 def main():
     projects = []
-    load_projects(projects)
+    get_projects(projects)
     print("Welcome to Pythonic Project Management")
     print(f"Loaded {len(projects)} from projects.txt")
+
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
-            pass
+            load_projects()
         elif choice == "S":
             pass
         elif choice == "D":
@@ -30,20 +31,39 @@ def main():
         print(MENU)
         choice = input(">>> ").upper()
 
+    save_input = input("Would you like to save to projects.txt? ").upper()
+    if save_input == "Y":
+        print("Saved")
+
     print("Thank you for using custom-built project management software.")
 
     # for project in projects:
     #     print(project)
 
 
-def load_projects(projects):
+def get_projects(projects):
+    # loads information from a present file
     in_file = open("projects.txt", "r")
     in_file.readline()
+    # ignores the first line full of irrelevant information
     for line in in_file:
         parts = line.strip().split("\t")
         project = Project(parts[0], parts[1], parts[2], parts[3], parts[4])
         projects.append(project)
     in_file.close()
+
+
+    """ User submitted projects """
+def load_projects(projects):
+    project_file = input("Enter project file name:  ")
+    project_file = open(project_file, "r")
+    project_file.readline()
+    for line_of_project in project_file:
+        parts_of_project = line_of_project.strip().split("\t")
+        user_project = Project(parts_of_project[0],parts_of_project[1],parts_of_project[2],parts_of_project[3],parts_of_project[4])
+        projects.append(user_project)
+    project_file.close()
+
 
 
 main()
