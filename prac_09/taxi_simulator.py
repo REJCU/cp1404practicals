725d7477a12aedbdd22bb7f0ccb6586dd87194c7
Ryan.E <ericsonryan34@gmail.com>
2024-11-23 16:44:23 +1000

    Restructered taxi_simulator.py

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -7,16 +7,12 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 
 MENU = "q)uit, c)hoose, d)rive"
 
-
-
 def main():
     bill_of_taxi = 0
-
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
 
     print(f"Let's drive!")
     print(MENU)
-
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
