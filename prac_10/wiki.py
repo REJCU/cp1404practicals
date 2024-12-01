59fec48aff2a016a69fdcd57239a397eb32dee17
Ryan.E <ericsonryan34@gmail.com>
2024-12-01 11:19:21 +1000

    Refactored search function for wkiki.py

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -8,8 +8,8 @@ def wiki_search():
     user_search = input("Enter page title: ")
     while user_search != "":
         try:
-            result = wikipedia.WikipediaPage(user_search)
-            print(f"{user_search}\nSummary:\n{result.summary}\n{result.pageid}")
+            result = wikipedia.page(user_search)
+            print(f"{user_search}\nSummary:\n{result.summary}\n{result}")
             user_search = input("search: ")
         except wikipedia.exceptions.DisambiguationError as error:
             print(f"We need a more specific title. Try one of the following, or a new search:\n{error.options}")
